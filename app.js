5372edb52edf2ae7fdf7cbda22b3d40f5e72e3e4
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-02-21 15:45:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// var a = Math.ceil(1.9);
+// var b = Math.round(2.5);
+// var c = Math.floor(2.1);
+// console.log(a,b,c)
+// function ceils(qiymat) {
+//    let nat;
+//     if (qiymat > 1 && qiymat < 2) {
+//         nat = 2;
+//     }
+//     if (qiymat > 2 && qiymat < 3) {
+//         nat = 3;
+//     }
+//     if (qiymat > 3 && qiymat < 4) {
+//         nat = 4;
+//     }
+//     if (qiymat > 4 && qiymat < 5) {
+//         nat = 5;
+//     }
+//     if (qiymat > 5 && qiymat < 6) {
+//         nat = 6;
+//     }
+//     return nat;
+// }
+// var res = ceils(1.4);
+// alert(res)
+// function zero(qiymat) {
+//     let nat;
+//     if (qiymat % 10 == 0) {
+//         nat = "bitta"
+//     }else if(qiymat % 10 != 0) {
+//         nat = "bitta";
+//     }
+//     return nat;
+// }
+// let jav = zero(100);
+// alert(jav)
+// function son(params) {
+//   let a = Math.random() * 3;
+//   let b = Math.random() * 3;
+//   let c = Math.random() * 3;
+//   a = Math.ceil(a);
+//   b = Math.ceil(b);
+//   c = Math.ceil(c);
+//   return a + "" + b + "" + c;
+// }
+// let b = son();
+// console.log("savollar " + b);
+// function r3() {
+//   return Math.ceil(Math.random() * 3);
+// }
+let m = [1,4,3,6,7,2]
+let n = 
+// function test() {
+//   let bir = r3();
+//   let ik = r3();
+//   let uch = r3();
+// }
---
+++ js/app.js
@@ -0,0 +1,118 @@
+function numvr(params) {
+  return Math.ceil(Math.random() * 10);
+}
+function rendom_var(params) {
+  return Math.ceil(Math.random() * 4);
+}
+var rendom_var = rendom_var();
+let sum_one = numvr();
+let sum_two = numvr();
+// end
+let var_one = numvr();
+let var_two = numvr();
+let var_three = numvr();
+let var_four = numvr();
+
+function inner_one(params) {
+  return (document.getElementById("variant-1").innerText = var_one);
+}
+function inner_two(params) {
+  return (document.getElementById("variant-2").innerText = var_two);
+}
+function inner_three(params) {
+  return (document.getElementById("variant-3").innerText = var_three);
+}
+function inner_four(params) {
+  return (document.getElementById("variant-4").innerText = var_four);
+}
+// if (sum_one != sum_two) {
+let summanat = sum_one + sum_two;
+if (
+  var_one != var_two &&
+  var_one != var_three &&
+  var_one != var_four &&
+  var_one != summanat
+) {
+  inner_one();
+} else {
+  window.location.reload();
+}
+if (
+  var_two != var_one &&
+  var_two != var_three &&
+  var_two != var_four &&
+  var_two != summanat
+) {
+  inner_two();
+} else {
+  window.location.reload();
+}
+if (
+  var_three != var_one &&
+  var_three != var_two &&
+  var_three != var_four &&
+  var_three != summanat
+) {
+  inner_three();
+} else {
+  window.location.reload();
+}
+if (
+  var_four != var_one &&
+  var_four != var_two &&
+  var_four != var_three &&
+  var_four != summanat
+) {
+  inner_four();
+}else{
+  
+  window.location.reload(true);
+}
+// }
+// if (sum_one != sum_two) {
+document.getElementById("num-one").innerText = sum_one;
+document.getElementById("num-two").innerText = sum_two;
+let sumnat = sum_one + sum_two;
+var var_ran = document.getElementById("variant-" + rendom_var);
+var_ran.innerText = sumnat;
+console.log(var_ran);
+// }
+
+//   all end
+
+function one_sub(params) {
+  if (1 == rendom_var) {
+    document.getElementById("con-box-one").style.backgroundColor = "green";
+    document.getElementById("con-box-one").style.color = "white";
+  } else {
+    document.getElementById("con-box-one").style.backgroundColor = "red";
+    document.getElementById("con-box-one").style.color = "white";
+  }
+}
+function two_sub(params) {
+  if (2 == rendom_var) {
+    document.getElementById("con-box-two").style.backgroundColor = "green";
+    document.getElementById("con-box-two").style.color = "white";
+  } else {
+    document.getElementById("con-box-two").style.backgroundColor = "red";
+    document.getElementById("con-box-two").style.color = "white";
+  }
+}
+function three_sub(params) {
+  if (3 == rendom_var) {
+    document.getElementById("con-box-three").style.backgroundColor = "green";
+    document.getElementById("con-box-three").style.color = "white";
+  } else {
+    document.getElementById("con-box-three").style.backgroundColor = "red";
+    document.getElementById("con-box-three").style.color = "white";
+  }
+}
+function four_sub(params) {
+  if (4 == rendom_var) {
+    document.getElementById("con-box-four").style.backgroundColor = "green";
+    document.getElementById("con-box-four").style.color = "white";
+  } else {
+    document.getElementById("con-box-four").style.backgroundColor = "red";
+    document.getElementById("con-box-four").style.color = "white";
+  }
+}
